9197aefaa38e23212635f4097019cd5d09d5e06c
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-07-12 23:00:42 -0300

    repository

---
+++ src/main/java/com/huandemberg/todosimple/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.huandemberg.todosimple.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.huandemberg.todosimple.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    
+    
+}
