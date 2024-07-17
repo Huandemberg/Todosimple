7aa95e428c2a8ad57997ca4316060c3ef40a7c6c
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-07-16 22:16:32 -0300

    treco

--- src/main/java/com/huandemberg/todosimple/services/UserService.java
+++ src/main/java/com/huandemberg/todosimple/services/UserService.java
@@ -33,7 +33,7 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
+        
         return obj;
     }
 
