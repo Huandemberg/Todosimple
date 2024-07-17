f77ea7762146bb5060e95ddd0c9a6f7a7323bd17
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-07-16 21:13:29 -0300

    de novo

--- src/main/java/com/huandemberg/todosimple/models/Task.java
+++ src/main/java/com/huandemberg/todosimple/models/Task.java
@@ -11,7 +11,6 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 @Table(name = Task.TABLE_NAME)
