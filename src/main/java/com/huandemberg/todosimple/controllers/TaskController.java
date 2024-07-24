bb49b7937db07138b828ccf76431ba1fc2d00e4d
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-07-23 23:27:48 -0300

    Algumas melhorias

--- src/main/java/com/huandemberg/todosimple/controllers/TaskController.java
+++ src/main/java/com/huandemberg/todosimple/controllers/TaskController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.huandemberg.todosimple.models.Task;
 import com.huandemberg.todosimple.services.TaskService;
+import com.huandemberg.todosimple.services.UserService;
 
 @RestController
 @RequestMapping("/task")
@@ -28,8 +30,12 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private UserService usersService;
+
     @GetMapping("/{id}")
     public ResponseEntity<Task> findById(@PathVariable Long id) {
+        
         Task obj = this.taskService.findById(id);
         return ResponseEntity.ok(obj);
     }
@@ -51,6 +57,7 @@ public class TaskController {
         return ResponseEntity.noContent().build();
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         this.taskService.delete(id);
         return ResponseEntity.noContent().build();
@@ -58,6 +65,7 @@ public class TaskController {
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Task>> findAllByuserId(@PathVariable Long userId) {
+        usersService.findById(userId);
         List<Task> objs = this.taskService.findAllByUserId(userId);
         return ResponseEntity.ok().body(objs);
     }
