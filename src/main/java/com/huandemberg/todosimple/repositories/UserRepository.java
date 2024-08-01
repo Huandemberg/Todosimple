a6b51c6369b2baf4a28cf43b3cd7936e2c4bca07
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-07-31 21:58:51 -0300

    JWTUtil e UserDetailsService

--- src/main/java/com/huandemberg/todosimple/repositories/UserRepository.java
+++ src/main/java/com/huandemberg/todosimple/repositories/UserRepository.java
@@ -3,12 +3,15 @@ package com.huandemberg.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.huandemberg.todosimple.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 
     
     
