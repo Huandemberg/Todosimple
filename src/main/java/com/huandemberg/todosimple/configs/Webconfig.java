e69ea1929614ab26a92831162d74e468bb66d8ac
Huandemberg <128419405+Huandemberg@users.noreply.github.com>
2024-08-14 17:00:36 -0300

    Refatoração do front

--- src/main/java/com/huandemberg/todosimple/configs/Webconfig.java
+++ src/main/java/com/huandemberg/todosimple/configs/Webconfig.java
@@ -10,7 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Webconfig implements WebMvcConfigurer {
     
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
     }
 
 }
